4a967d32c2e3eb9d1bc0fe316685ec290e173753
arjuntaliyan18 <arjuntaliyan86@gmail.com>
2022-06-05 22:49:19 +0530

    addnumber.cpp file added

---
+++ addnumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int Sum (int n){
+    if (n== 0)
+    {
+        return 0;
+    }
+    int prevsum = Sum (n-1);
+    return n + prevsum;
+}
+int main () {
+    int n;
+    cin>>n;
+    cout<<Sum(n)<<endl;
+    return 0;
+    
+
+    
+}
