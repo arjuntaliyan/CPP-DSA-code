ca2da9b52491e10123fd5ed2e7ad171f5d959098
Arjun Taliyan <105619051+arjuntaliyan@users.noreply.github.com>
2022-09-06 21:08:27 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,22 @@
+// Add all number till n
+
+#include<iostream>
+using namespace std;
+
+int Sum(int n)
+{
+    if(n==0){
+        return 0;
+    }
+
+    int prevsum = Sum(n-1);
+    return n+prevsum;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<Sum(n)<<endl;
+    return 0;
+}
