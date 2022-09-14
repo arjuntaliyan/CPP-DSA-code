011abb41e482e8582f8fe8ec5396a25b4b76603e
Arjun Taliyan <105619051+arjuntaliyan@users.noreply.github.com>
2022-09-14 22:18:55 +0530

    Add files via upload

---
+++ substring.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void subSeq (string s, string ans)
+{
+    if (s.length() == 0)
+    {
+        cout<< ans << endl;
+        return ;
+    }
+    char ch = s[0];
+    string ros = s.substr(1);
+
+    subSeq (ros,ans);
+    subSeq (ros, ans + ch);
+}
+
+int main ()
+{
+    subSeq("ABC", " ");
+    cout<<endl;
+    return 0;
+}
